7a89b48ae2ec1e1023c2f76cc889d82e7eb56515
Taylor Everts <tayloreverts@quickenloans.com>
2018-11-20 11:40:52 -0500

    commit1

---
+++ AlexaDontDie/AlexaDontDie/Pseudocode.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlexaDontDie
+{
+    class Pseudocode
+    {
+        /*  var iventory = bool
+            var energy = int
+            var hasWater = bool
+
+            DAY 2
+
+            if(i == true){
+            question1 = "the tarp you set up last night collected water" do you ____? => y = ___, n = ____
+            else{
+            question1 = "you are thirsty. do you look for water"? => y = energy-- hasWater = true, n = hasWater = false 
+            }
+
+            question2 = "you're hungry. do you look for food"? => y = energy++ n = energy--
+
+            if(i == true){
+            question3 = "you have a lighter. do you make a fire?" => y = energy++, n = energy--
+            }
+            else{
+            question3 = "do you make a fire?" => y = energy--, n = energy----,
+            }
+
+            question4 = "you hear shuffling and grunts outside, do you investigate?" 
+            y=(
+            if(energy < 4)
+            game over
+            else{
+            its a bear and you run away or something
+            energy --)
+            n = nothing happens
+
+            
+            DAY 3
+            if(energy < 3 or hasWater = false)
+            {
+            dead
+            }
+            else{
+            hears helicopters
+            if(energy < 4)
+            dead
+            {
+            else 
+            rescued
+            }
+        */
+    }
+}
