0920fc9b57c05d688a0a0ebfb041692f39760f91
Blake McMillan Shaw <blakeshaw@quickenloans.com>
2018-11-20 10:50:40 -0500

    Made template.

---
+++ AlexaDontDie/AlexaDontDie/Function.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Alexa.NET.Response;
+using Alexa.NET.Request;
+using Alexa.NET.Request.Type;
+using Alexa.NET.Response.Directive;
+using Amazon.Lambda.Core;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace AlexaDontDie
+{
+    public class Function
+    {
+        private SkillResponse response = null;
+        private ILambdaContext context = null;
+
+        /// <summary>
+        /// Application entry point
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="ctx"></param>
+        /// <returns></returns>
+        public SkillResponse FunctionHandler(SkillRequest input, ILambdaContext ctx)
+        {
+            context = ctx;
+            try
+            {
+                response = new SkillResponse();
+                response.Response = new ResponseBody();
+                response.Response.ShouldEndSession = false;
+                response.Version = "1.0";
+
+                Log(JsonConvert.SerializeObject(response));
+                return response;
+            }
+            catch (Exception ex)
+            {
+                Log($"error : {ex.Message}");
+            }
+            return null;
+        }
+
+        private void Log(string text)
+        {
+            if (context != null)
+            {
+                context.Logger.LogLine(text);
+            }
+        }
+    }
+}
+
+
