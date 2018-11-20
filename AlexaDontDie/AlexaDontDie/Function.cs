d0f526530cf6a4cbb1bae65beaca825d376a9141
Blake McMillan Shaw <blakeshaw@quickenloans.com>
2018-11-20 11:25:56 -0500

    Added process launch

--- AlexaDontDie/AlexaDontDie/Function.cs
+++ AlexaDontDie/AlexaDontDie/Function.cs
@@ -3,6 +3,7 @@ using Newtonsoft.Json;
 using Alexa.NET.Response;
 using Alexa.NET.Request;
 using Amazon.Lambda.Core;
+using Alexa.NET.Request.Type;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
@@ -30,6 +31,11 @@ namespace AlexaDontDie
                 response.Response.ShouldEndSession = false;
                 response.Version = "1.0";
 
+                if (input.GetRequestType() == typeof(LaunchRequest))
+                {
+                    ProcessLaunchRequest(response.Response);
+                }
+
                 Log(JsonConvert.SerializeObject(response));
                 return response;
             }
@@ -38,6 +44,19 @@ namespace AlexaDontDie
                 Log($"error : {ex.Message}");
             }
             return null;
+        }
+
+        private void ProcessLaunchRequest(ResponseBody response)
+        {
+            IOutputSpeech innerResponse = new SsmlOutputSpeech();
+            (innerResponse as SsmlOutputSpeech).Ssml = "<speak>Welcome to Don't Die. Try not to die.</speak>";
+            response.OutputSpeech = innerResponse;
+            IOutputSpeech prompt = new PlainTextOutputSpeech();
+            (prompt as PlainTextOutputSpeech).Text = "Say yes or no";
+            response.Reprompt = new Reprompt()
+            {
+                OutputSpeech = prompt
+            };
         }
 
         private void Log(string text)
