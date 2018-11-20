041e0ed18f2e8de2cf9343a885017d8ff44de2fc
Blake McMillan Shaw <blakeshaw@quickenloans.com>
2018-11-20 13:08:25 -0500

    Added processing intents and yes/no functions

--- AlexaDontDie/AlexaDontDie/Function.cs
+++ AlexaDontDie/AlexaDontDie/Function.cs
@@ -4,6 +4,8 @@ using Alexa.NET.Response;
 using Alexa.NET.Request;
 using Amazon.Lambda.Core;
 using Alexa.NET.Request.Type;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
@@ -14,6 +16,7 @@ namespace AlexaDontDie
     {
         private SkillResponse response = null;
         private ILambdaContext context = null;
+        public static Resource resource = new Resource();
 
         /// <summary>
         /// Application entry point
@@ -26,8 +29,10 @@ namespace AlexaDontDie
             context = ctx;
             try
             {
-                response = new SkillResponse();
-                response.Response = new ResponseBody();
+                response = new SkillResponse
+                {
+                    Response = new ResponseBody()
+                };
                 response.Response.ShouldEndSession = false;
                 response.Version = "1.0";
 
@@ -35,7 +40,10 @@ namespace AlexaDontDie
                 {
                     ProcessLaunchRequest(response.Response);
                 }
-
+                else if (input.GetRequestType() == typeof(IntentRequest))
+                {
+                    ProcessIntentRequest(input);
+                }
                 Log(JsonConvert.SerializeObject(response));
                 return response;
             }
@@ -49,16 +57,90 @@ namespace AlexaDontDie
         private void ProcessLaunchRequest(ResponseBody response)
         {
             IOutputSpeech innerResponse = new SsmlOutputSpeech();
-            (innerResponse as SsmlOutputSpeech).Ssml = "<speak>Welcome to Don't Die. Try not to die.<audio src='soundbank://soundlibrary/animals/amzn_sfx_bear_roar_grumble_01'/></speak>";
+            (innerResponse as SsmlOutputSpeech).Ssml = SsmlDecorate(resource.LaunchMessage);
             response.OutputSpeech = innerResponse;
             IOutputSpeech prompt = new PlainTextOutputSpeech();
-            (prompt as PlainTextOutputSpeech).Text = "Say yes or no";
+            (prompt as PlainTextOutputSpeech).Text = resource.LaunchMessageReprompt;
             response.Reprompt = new Reprompt()
             {
                 OutputSpeech = prompt
             };
         }
 
+        private IOutputSpeech ProcessIntentRequest(SkillRequest input)
+        {
+            IOutputSpeech innerResponse = new PlainTextOutputSpeech();
+
+            var intentName = ((IntentRequest)input.Request).Intent.Name;
+
+            switch (intentName)
+            {
+                case BuiltInIntent.Yes:
+                    innerResponse = new SsmlOutputSpeech();
+                    (innerResponse as SsmlOutputSpeech).Ssml = ProcessYesIntent(input.Session.Attributes);
+                    break;
+                case BuiltInIntent.No:
+                    innerResponse = new SsmlOutputSpeech();
+                    (innerResponse as SsmlOutputSpeech).Ssml = ProcessNoIntent(input.Session.Attributes);
+                    break;
+                case BuiltInIntent.Cancel:
+                case BuiltInIntent.Stop:
+                    (innerResponse as PlainTextOutputSpeech).Text = resource.StopMessage;
+                    response.Response.ShouldEndSession = true;
+                    break;
+                case BuiltInIntent.Help:
+                    (innerResponse as PlainTextOutputSpeech).Text = resource.HelpMessage;
+                    break;
+                default:
+                    (innerResponse as PlainTextOutputSpeech).Text = resource.HelpReprompt;
+                    break;
+            }
+            if (innerResponse.Type == "SSML")
+            {
+                BuildCard(resource.SkillName, (innerResponse as SsmlOutputSpeech).Ssml);
+                (innerResponse as SsmlOutputSpeech).Ssml = SsmlDecorate((innerResponse as SsmlOutputSpeech).Ssml);
+            }
+
+            return innerResponse;
+        }
+
+        private string ProcessYesIntent(Dictionary<string, object> attributes)
+        {
+            if (attributes.Count == 0)
+            {
+                attributes.Add("Question", 2);
+                response.SessionAttributes = attributes;
+                return "Excellent!";
+            }
+            return null;
+        }
+
+        private string ProcessNoIntent(Dictionary<string, object> attributes)
+        {
+            if (attributes.Count == 0)
+            {
+                response.Response.ShouldEndSession = true;
+                return "Boo!";
+            }
+            return null;
+        }
+
+        private string SsmlDecorate(string speech)
+        {
+            return $"<speak>{speech}</speak>";
+        }
+        private void BuildCard(string title, string output)
+        {
+            if (!String.IsNullOrEmpty(output))
+            {
+                output = Regex.Replace(output, @"<.*?>", "");
+                response.Response.Card = new SimpleCard()
+                {
+                    Title = title,
+                    Content = output,
+                };
+            };
+        }
         private void Log(string text)
         {
             if (context != null)
---
+++ AlexaDontDie/AlexaDontDie/Resource.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlexaDontDie
+{
+    public class Resource
+    {
+        public string Language { get; set; } = "en-US";
+        public string SkillName { get; set; } = "Don't Die";
+        public string LaunchMessage { get; set; } = "Welcome to Don't Die where you try not to die.<audio src='soundbank://soundlibrary/animals/amzn_sfx_bear_roar_grumble_01'/> Would you like to try your luck?";
+        public string LaunchMessageReprompt { get; set; } = "Say yes or no";
+        public string AskMessage { get; set; } = "Yes or no?";
+        public string HelpMessage { get; set; } = "Just say yes or no. Say help to hear the prompt again. You can say exit at any time to exit.";
+        public string HelpReprompt { get; set; } = "Yes, no, help, or exit.";
+        public string StopMessage { get; set; } = "Safe adventures!";
+    }
+}
